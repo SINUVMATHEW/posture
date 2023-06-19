7217a9c925e3fae3db57979cb355872a2d162f45
SINU V MATHEW <63470998+SINUVMATHEW@users.noreply.github.com>
2023-06-19 09:53:58 +0530

    file uploaded

---
+++ sound.py
@@ -0,0 +1,7 @@
+import winsound
+# Set frequency to 2000 Hertz
+frequency = 2000
+# Set duration to 1500 milliseconds (1.5 seconds)
+duration = 20000
+# Make beep sound on Windows
+winsound.Beep(frequency, duration)
